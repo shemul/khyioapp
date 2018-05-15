6acb66de52b29a44a113a239f2dc83c02a3babfa
shemul <theshemul@gmail.com>
2018-05-15 09:52:01 +0000

    removed unused codes

--- app.js
+++ app.js
@@ -1,64 +1,31 @@
 var sqlite3 = require('sqlite3').verbose();
-var fs = require('fs');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var express = require('express')
 var app = express();
-var fs = require('fs');
-
 var http = require('http');
-var https = require('https');
-var port = {
-	'http': 8080,
-	'https': 5556
+
+var normalizePort = function(val) {
+	var port;
+	port = parseInt(val, 10);
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
 };
 
-//var privateKey  = fs.readFileSync('/home/theshemul/ssl.key', 'utf8');
-//var certificate = fs.readFileSync('/home/theshemul/ssl.cert', 'utf8');
-//var credentials = {key: privateKey, cert: certificate};
+var port = normalizePort(process.env.PORT || '8080');
+var ip = process.env.IP
 
 var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-//var mongoose = require('mongoose');
-
 
 app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-/*=============================================
-=            		Database	               =
-=============================================*/
-
-//mongoose.connect('mongodb://localhost/khyio');
-//var Schema = mongoose.Schema;
-
-// create a schema
-// var userSchema = new Schema({
-//   username: { type: String, required: true },
-//   password: { type: String, required: true },
-//   created_at: Date,
-//   updated_at: Date
-// });
-
-// on every save, add the date
-// userSchema.pre('save', function(next) {
-//   // get the current date
-//   var currentDate = new Date();
-
-//   // change the updated_at field to current date
-//   this.updated_at = currentDate;
-
-//   // if created_at doesn't exist, add to that field
-//   if (!this.created_at)
-//     this.created_at = currentDate;
-
-//   next();
-// });
-
-//var User = mongoose.model('aiub', userSchema);
-
-/*=====  End of the Database  ======*/
 
 app.get('/dict/:word', function(req, res) {
 
@@ -82,10 +49,6 @@ app.get('/dict/:word', function(req, res) {
 				found: false,
 				status: "Not Found"
 			}
-
-			//fs.appendFile("top_list.txt", req.params.word + '\r\n', function(err) {
-			//}); 
-
 			res.send(notFound);
 		}
 
@@ -95,33 +58,6 @@ app.get('/dict/:word', function(req, res) {
 	db.close();
 
 })
-
-
-
-// app.post('/stat', function(req, res) {
-//     var user = req.body.user;
-//     var pass = req.body.pass
-
-//     var client = new User({
-//       username: user,
-//       password: pass 
-//     });
-
-//     client.save(function(err) {
-//       if (err) {
-//       	console.log(err);
-//       } else {
-// 		console.log('User saved successfully!');
-// 		res.send("Success");
-//       }
-//     });
-
-// });
-
-httpServer.listen(port.http, function() {
-	console.log("Http Service is running on port " + port.http);
+httpServer.listen(port, function() {
+	console.log("Http Service is running on port " + port);
 });
-
-// httpsServer.listen(port.https, function() {
-//  	console.log("Https Service is running on port " + port.https);
-// });
